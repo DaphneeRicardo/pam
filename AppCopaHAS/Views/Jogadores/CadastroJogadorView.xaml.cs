e9b731fd30b9cfb6b349eb64b378be11b7524998
DaphneeRicardo <Daphnericardo2608@gmail.com>
2026-06-22 09:27:19 -0300

    ultima atividade

---
+++ AppCopaHAS/Views/Jogadores/CadastroJogadorView.xaml.cs
@@ -0,0 +1,9 @@
+namespace AppCopaHAS.Views.Jogadores;
+
+public partial class CadastrJogadorView : ContentPage
+{
+	public CadastrJogadorView()
+	{
+		InitializeComponent();
+	}
+}
---
+++ AppCopaHAS/Views/Jogadores/ListagemJogadorVierw.xaml.cs
@@ -0,0 +1,12 @@
+namespace AppCopaHAS.Views.Jogadores;
+
+public partial class ListagemJogadorVierw : ContentPage
+{
+	public ListagemJogadorVierw()
+	{
+		InitializeComponent();
+
+        this.BindingContext = new ListagemJogadorVierw();
+
+    }
+}
